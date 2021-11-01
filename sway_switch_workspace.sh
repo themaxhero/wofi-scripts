957dda268d6e16568263c9e46d9d2ddcf76be1d3
Marcelo Amancio de Lima Santos <maxherogamer.contato@gmail.com>
2021-11-01 11:10:51 -0300

    Update script to work correctly with swaymsg

--- sway_switch_workspace.sh
+++ sway_switch_workspace.sh
@@ -5,14 +5,14 @@ _PROGRAM=wofi
 
 function gen_workspaces()
 {
-    $_MSG_BIN -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g' | sort -n
+    $_MSG_BIN -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g; s/"name": "//g; s/"//g' | sort -n
 }
 
 _WORKSPACE=$( (echo empty; gen_workspaces)  | $_PROGRAM -dmenu -p "Select workspace:")
 
 if [ x"empty" = x"${_WORKSPACE}" ]
 then
-    i3_empty_workspace.sh
+    wofi_empty_workspace.sh
 elif [ -n "${_WORKSPACE}" ]
 then
     $_MSG_BIN workspace "${_WORKSPACE}"
